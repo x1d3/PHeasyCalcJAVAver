287310f475b22e15f1aa7826dad8573056e1fd74
x13d <dimasik7557@gmail.com>
2024-03-03 18:52:57 +0300

    added activity blanks

---
+++ app/src/main/java/com/example/pheasycalc/A_activity_1.java
@@ -0,0 +1,14 @@
+package com.example.pheasycalc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class A_activity_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aactivity1);
+    }
+}
---
+++ app/src/main/java/com/example/pheasycalc/P_activity_1.java
@@ -0,0 +1,14 @@
+package com.example.pheasycalc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class P_activity_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pactivity1);
+    }
+}
