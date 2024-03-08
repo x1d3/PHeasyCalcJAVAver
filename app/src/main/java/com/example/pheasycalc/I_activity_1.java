1f225d1a5c03a8163b106955df119857a57fd611
x1d3 <dimasik7557@gmail.com>
2024-03-09 00:58:49 +0300

    now the calculation of the current strength has full functionality

--- app/src/main/java/com/example/pheasycalc/I_activity_1.java
+++ app/src/main/java/com/example/pheasycalc/I_activity_1.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class I_activity_1 extends AppCompatActivity {
 
@@ -13,13 +17,51 @@ public class I_activity_1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iactivity1);
 
-        Button button7 = findViewById(R.id.button7);
-        button7.setOnClickListener(new View.OnClickListener() {
+        // Получение переданных данных из Intent
+        Intent intent = getIntent();
+
+        // Отображение единиц измерения в Spinner
+        Spinner spinner = findViewById(R.id.spinner23);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_items, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        // Отображение единиц времени в Spinner
+        Spinner spinnerTime = findViewById(R.id.spinner2);
+
+        ArrayAdapter<CharSequence> adapterTime = ArrayAdapter.createFromResource(this,
+                R.array.spinner_times, android.R.layout.simple_spinner_item);
+        adapterTime.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerTime.setAdapter(adapterTime);
+
+        // Обработчик события для кнопки "Далее"
+        Button nextButton = findViewById(R.id.button7);
+        nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(I_activity_1.this, I_activity_2.class);
-                startActivity(intent);
+                // Выполнение расчетов и переход к следующей активности
+                calculateAndNavigate();
             }
         });
+    }
+
+    private void calculateAndNavigate() {
+        // Получение данных из EditText
+        EditText editTextNumber2 = findViewById(R.id.editTextNumber2);
+        EditText editTextNumber22 = findViewById(R.id.editTextNumber22);
+
+        // Преобразование данных в числа
+        double value2 = Double.parseDouble(editTextNumber2.getText().toString());
+        double value22 = Double.parseDouble(editTextNumber22.getText().toString());
+
+        // Выполнение расчетов
+        double calculatedResult = value2 / value22;
+
+        // Переход к следующей активности и передача результата
+        Intent intent = new Intent(I_activity_1.this, I_activity_2.class);
+        intent.putExtra("result", calculatedResult);
+        startActivity(intent);
     }
 }
