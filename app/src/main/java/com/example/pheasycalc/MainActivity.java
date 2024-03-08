b0162b295e8e8be22503f025cfb3d76d79e3b019
x1d3 <dimasik7557@gmail.com>
2024-03-08 23:08:14 +0300

    we have configured the functionality of the menu

--- app/src/main/java/com/example/pheasycalc/MainActivity.java
+++ app/src/main/java/com/example/pheasycalc/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.pheasycalc;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,69 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button6 = findViewById(R.id.button6);
+        Button button = findViewById(R.id.button);
+        Button button5 = findViewById(R.id.button5);
+        Button button3 = findViewById(R.id.button3);
+        Button button1 = findViewById(R.id.button1);
+        Button button4 = findViewById(R.id.button4);
+        Button button2 = findViewById(R.id.button2);
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, I_activity_1.class);
+                startActivity(intent);
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, U_activity_1.class);
+                startActivity(intent);
+            }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, A_activity_1.class);
+                startActivity(intent);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, P_activity_1.class);
+                startActivity(intent);
+            }
+        });
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, R_activity_1.class);
+                startActivity(intent);
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Q_activity_1.class);
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, OM_activity_1.class);
+                startActivity(intent);
+            }
+        });
     }
 }
