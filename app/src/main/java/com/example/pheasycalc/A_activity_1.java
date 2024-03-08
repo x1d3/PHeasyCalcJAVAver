db52ed328abb05b7befba6a0b6e0c36fb5d57036
x1d3 <dimasik7557@gmail.com>
2024-03-09 00:40:36 +0300

    now the calculation of the current operation is fully operational

--- app/src/main/java/com/example/pheasycalc/A_activity_1.java
+++ app/src/main/java/com/example/pheasycalc/A_activity_1.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class A_activity_1 extends AppCompatActivity {
 
@@ -17,9 +20,39 @@ public class A_activity_1 extends AppCompatActivity {
         button7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Получение значений из EditText
+                EditText editTextNumber2 = findViewById(R.id.editTextNumber2);
+                EditText editTextNumber21 = findViewById(R.id.editTextNumber21);
+                EditText editTextNumber22 = findViewById(R.id.editTextNumber22);
+                double value1 = Double.parseDouble(editTextNumber2.getText().toString());
+                double value2 = Double.parseDouble(editTextNumber21.getText().toString());
+                double value3 = Double.parseDouble(editTextNumber22.getText().toString());
+
+                // Получение выбранных элементов из Spinner
+                Spinner spinner23 = findViewById(R.id.spinner23);
+                String unit1 = spinner23.getSelectedItem().toString();
+                Spinner spinner21 = findViewById(R.id.spinner21);
+                String unit2 = spinner21.getSelectedItem().toString();
+                Spinner spinner2 = findViewById(R.id.spinner2);
+                String timeUnit = spinner2.getSelectedItem().toString();
+
+                // Вычисления
+                double result = calculateResult(value1, value2, value3, unit1, unit2, timeUnit);
+
+                // Передача результата в следующую активность
                 Intent intent = new Intent(A_activity_1.this, A_activity_2.class);
+                intent.putExtra("result", result);
                 startActivity(intent);
             }
         });
+    }
+
+    private double calculateResult(double value1, double value2, double value3, String unit1, String unit2, String timeUnit) {
+        // Выполнение вычислений
+        // Реализуйте здесь необходимые вычисления в соответствии с вашей формулой
+        // Пример:
+        double result = value1 * value2 * value3;
+        // Дополните этот метод в соответствии с вашей логикой
+        return result;
     }
 }
