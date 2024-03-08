f4a918e57ee56d529d8e4106d5aa0de84bf28786
x1d3 <dimasik7557@gmail.com>
2024-03-09 00:17:15 +0300

    Writing code to switch from the second activities to the main activity

--- app/src/main/java/com/example/pheasycalc/P_activity_2.java
+++ app/src/main/java/com/example/pheasycalc/P_activity_2.java
@@ -1,8 +1,10 @@
 package com.example.pheasycalc;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class P_activity_2 extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class P_activity_2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pactivity2);
+
+        Button button9 = findViewById(R.id.button9);
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Создание Intent для перехода на MainActivity
+                Intent intent = new Intent(P_activity_2.this, MainActivity.class);
+                // Запуск MainActivity
+                startActivity(intent);
+            }
+        });
     }
 }
